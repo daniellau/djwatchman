1414dd31feb29a19917e811aab38032d4417825c
Daniel Lau <daniell@ibethel.org>
2014-08-04 12:39:33 -0700

    integer instead of string

--- lib/djwatchman.rb
+++ lib/djwatchman.rb
@@ -4,7 +4,7 @@ module Djwatchman
   class DelayedJobMonitor
     def self.run
       begin
-        count = Delayed::Job.where("created_at < ?", (ENV['DJWATCHMAN_MINUTES'] || 60).minutes.ago).count
+        count = Delayed::Job.where("created_at < ?", (ENV['DJWATCHMAN_MINUTES'].to_i || 60).minutes.ago).count
         if count > 0
           raise "There are #{count} unprocessed delayed jobs."
         end
